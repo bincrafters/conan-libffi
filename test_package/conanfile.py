6e8f1af9b03baad829e4112b92c247f35ec3e57a
Steve Mokris <smokris@softpixel.com>
2017-12-17 22:18:18 -0500

    Replace conanfile_directory (deprecated in Conan 0.30.0) with source_folder.

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,7 +4,7 @@ class LibffiTestConan(ConanFile):
     generators = 'qbs'
 
     def build(self):
-        self.run('qbs -f "%s"' % self.conanfile_directory);
+        self.run('qbs -f "%s"' % self.source_folder)
 
     def imports(self):
         self.copy('*.dylib', dst='bin', src='lib')
